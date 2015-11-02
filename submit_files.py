fec7c3aab4049f2730f6a72b47cb762d6a239db7
Marissa Melen <marissa@heatseq.stanford.edu>
2015-11-02 13:24:38 -0800

    using proper names for items

--- submit_files.py
+++ submit_files.py
@@ -44,6 +44,10 @@ class NewFile():
         if dictionary.get("controlled_by"):
             control = dictionary.pop("controlled_by")
             self.post_input["controlled_by"] = control.split(",")
+        # get aliases list
+        if dictionary.get("aliases"):
+            control = dictionary.pop("aliases")
+            self.post_input["aliases"] = control.split(",")
         # make flowcell dict
         flowcell_dict = {}
         for val in ["lane", "barcode", "flowcell", "machine"]:
@@ -51,11 +55,11 @@ class NewFile():
         # make post_input dict
         path = dictionary.pop("file_path")
         for key in dictionary.keys():
-            if key == "file_submitted_name":
-                if any(dictionary.get("file_submitted_name")):
-                    self.post_input["file_submitted_name"] = dictionary["file_submitted_name"]
+            if key == "submitted_file_name":
+                if any(dictionary.get("submitted_file_name")):
+                    self.post_input["submitted_file_name"] = dictionary["submitted_file_name"]
                 else:
-                    self.post_input["file_submitted_name"] = path.rsplit("/", 1)[-1]
+                    self.post_input["submitted_file_name"] = path.rsplit("/", 1)[-1]
             else:
                 if dictionary.get(key):
                     self.post_input[key] = dictionary[key]
@@ -75,10 +79,9 @@ class NewFile():
         # POST metadata
         print("Submitting metadata.")
         r = encodedcc.new_ENCODE(connection, "files", self.post_input)
-        item = r["@graph"][0]
         #####################
         # POST file to S3
-
+        '''item = r["@graph"][0]
         creds = item['upload_credentials']
         env = os.environ.copy()
         env.update({
@@ -93,7 +96,7 @@ class NewFile():
         subprocess.check_call(['aws', 's3', 'cp', path, creds['upload_url']], env=env)
         end = time.time()
         duration = end - start
-        print("Uploaded in %.2f seconds" % duration)
+        print("Uploaded in %.2f seconds" % duration)'''
 
 ######################
 # Main
