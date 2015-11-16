42399563e3f3aeda028f682ca64cb059063b4346
Marissa Melen <marissa@heatseq.stanford.edu>
2015-11-16 09:48:30 -0800

    switching to python 2.7 for server uploads

---
+++ ENCODE_submit_files_2_7.py
@@ -0,0 +1,214 @@
+import hashlib
+import os.path
+import subprocess
+import time
+import csv
+import argparse
+import logging
+import json
+import sys
+import requests
+import gzip
+from io import BytesIO
+from urllib.parse import urljoin
+from urllib.parse import quote
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename="log.txt", filemode="w", format='%(message)s')
+#############################
+# Set defaults
+
+
+def getArgs():
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        )
+    parser.add_argument('--infile',
+                        help="TSV file with data, needs headers")
+    parser.add_argument('--key',
+                        default='default',
+                        help="The keypair identifier from the keyfile.  \
+                        Default is --key=default")
+    parser.add_argument('--keyfile',
+                        default=os.path.expanduser("~/keypairs.json"),
+                        help="The keypair file.  Default is --keyfile=%s" % (os.path.expanduser("~/keypairs.json")))
+    parser.add_argument('--debug',
+                        default=False,
+                        action='store_true',
+                        help="Print debug messages.  Default is False.")
+    parser.add_argument('--update',
+                        default=False,
+                        action="store_true",
+                        help="Allows script to update, default is false")
+    args = parser.parse_args()
+    return args
+
+
+class ENC_Key:
+    def __init__(self, keyfile, keyname):
+        keys_f = open(keyfile, 'r')
+        keys_json_string = keys_f.read()
+        keys_f.close()
+        keys = json.loads(keys_json_string)
+        key_dict = keys[keyname]
+        self.authid = key_dict['key']
+        self.authpw = key_dict['secret']
+        self.server = key_dict['server']
+        if not self.server.endswith("/"):
+            self.server += "/"
+
+
+class ENC_Connection(object):
+    def __init__(self, key):
+        self.headers = {'content-type': 'application/json'}
+        self.server = key.server
+        self.auth = (key.authid, key.authpw)
+
+
+class NewFile():
+    def __init__(self, dictionary, connection):
+        self.post_input = {}
+        self.connection = connection
+        # get controlled_by list
+        if dictionary.get("controlled_by"):
+            control = dictionary.pop("controlled_by")
+            self.post_input["controlled_by"] = control.split(",")
+
+        # get aliases list
+        if dictionary.get("aliases"):
+            alias = dictionary.pop("aliases")
+            self.post_input["aliases"] = alias.split(",")
+
+        # make flowcell dict
+        flowcell_dict = {}
+        for val in ["lane", "barcode", "flowcell", "machine"]:
+            flowcell_dict[val] = dictionary.pop(val)
+        # add flowcell_details to post_input
+        self.post_input["flowcell_details"] = [flowcell_dict]
+
+        # calculate md5sum
+        md5sum = hashlib.md5()
+        path = dictionary.pop("file_path")
+        with open(path, "rb") as f:
+            for chunk in iter(lambda: f.read(1024*1024), b''):
+                md5sum.update(chunk)
+        # add md5sum to post_input
+        self.post_input["md5sum"] = md5sum.hexdigest()
+
+        # fill in rest of post_input
+        for key in dictionary.keys():
+            if key == "submitted_file_name":
+                if any(dictionary.get("submitted_file_name")):
+                    self.post_input["submitted_file_name"] = dictionary["submitted_file_name"]
+                else:
+                    self.post_input["submitted_file_name"] = path.rsplit("/", 1)[-1]
+            else:
+                if dictionary.get(key):
+                    self.post_input[key] = dictionary[key]
+
+        # if fastq get the read_length
+        if dictionary.get("file_format") == "fastq":
+            for header, sequence, qual_header, quality in self.fastq_read(self.connection, filename=path):
+                sequence = sequence.decode("UTF-8")
+                read_length = len(sequence)
+            self.post_input["read_length"] = read_length
+
+    def post_file(self):
+        ####################
+        # POST metadata
+        r = self.new_ENCODE(self.connection, "files", self.post_input)
+        if r.get("@graph"):
+            #####################
+            # POST file to S3
+            item = r["@graph"][0]
+            creds = item['upload_credentials']
+            env = os.environ.copy()
+            env.update({
+                'AWS_ACCESS_KEY_ID': creds['access_key'],
+                'AWS_SECRET_ACCESS_KEY': creds['secret_key'],
+                'AWS_SECURITY_TOKEN': creds['session_token'],
+            })
+            print "Uploading file"
+            path = self.data["file_path"]
+            print path
+            start = time.time()
+            subprocess.check_call(['aws', 's3', 'cp', path, creds['upload_url']], env=env)
+            end = time.time()
+            duration = end - start
+            print "Uploaded in %.2f seconds" % duration
+        else:
+            print "Couldn't upload to S3"
+            print r
+
+    def new_ENCODE(self, connection, collection_name, post_input):
+        '''POST an ENCODE object as JSON and return the response JSON
+        '''
+        if isinstance(post_input, dict):
+            json_payload = json.dumps(post_input)
+        elif isinstance(post_input, str):
+            json_payload = post_input
+        else:
+            print('Datatype to POST is not string or dict.', file=sys.stderr)
+        url = urljoin(connection.server, collection_name)
+        logging.debug("POST URL : %s" % (url))
+        logging.debug("POST data: %s" % (json.dumps(post_input,
+                                         sort_keys=True, indent=4,
+                                         separators=(',', ': '))))
+        response = requests.post(url, auth=connection.auth,
+                                 headers=connection.headers, data=json_payload)
+        logging.debug("POST RESPONSE: %s" % (json.dumps(response.json(),
+                                             indent=4, separators=(',', ': '))))
+        if not response.status_code == 201:
+            logging.warning('POST failure. Response = %s' % (response.text))
+        logging.debug("Return object: %s" % (json.dumps(response.json(),
+                                             sort_keys=True, indent=4,
+                                             separators=(',', ': '))))
+        return response.json()
+
+    def fastq_read(self, connection, uri=None, filename=None, reads=1):
+        '''Read a few fastq records
+        '''
+        # https://github.com/detrout/encode3-curation/blob/master/validate_encode3_aliases.py#L290
+        # originally written by Diane Trout
+        
+        # Reasonable power of 2 greater than 50 + 100 + 5 + 100
+        # which is roughly what a single fastq read is.
+        if uri:
+            BLOCK_SIZE = 512
+            url = urljoin(connection.server, quote(uri))
+            data = requests.get(url, auth=connection.auth, stream=True)
+            block = BytesIO(next(data.iter_content(BLOCK_SIZE * reads)))
+            compressed = gzip.GzipFile(None, 'r', fileobj=block)
+        elif filename:
+            compressed = gzip.GzipFile(filename, 'r')
+        else:
+            print "No url or filename provided! Cannot access file!"
+            return
+        for i in range(reads):
+            header = compressed.readline().rstrip()
+            sequence = compressed.readline().rstrip()
+            qual_header = compressed.readline().rstrip()
+            quality = compressed.readline().rstrip()
+            yield (header, sequence, qual_header, quality)
+
+
+def main():
+    args = getArgs()
+    key = ENC_Key(args.keyfile, args.key)
+    connection = ENC_Connection(key)
+    print "Running on", connection.server
+    if args.update:
+        print "This is an UPDATE run, data will be changed"
+    else:
+        print "This is a TEST run, nothing gets altered"
+    with open(args.infile, "r") as tsvfile:
+        reader = csv.DictReader(tsvfile, delimiter='\t')
+        for row in reader:
+            newF = NewFile(row, connection)
+            if args.update:
+                newF.post_file()
+            print "Data to POST: ", newF.post_input
+
+if __name__ == '__main__':
+    main()
