678d8c5952bbe7c0f704c6a128da4c1ff22ce9b7
Marissa Melen <marissa@heatseq.stanford.edu>
2015-12-14 16:09:34 -0800

    —method disabled (for now) script should identify type of setup using basic rules, objects that don’t fit the rules are ignored

--- 2562_controlled_by_backfill.py
+++ 2562_controlled_by_backfill.py
@@ -15,14 +15,13 @@ def getArgs():
         description=__doc__, epilog=EPILOG,
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
-    parser.add_argument('--method',
-                        help="'single' = there is only one replicate in the control, \
-                              'multi' = one control with same number of replicates as experiment has replicates, \
-                              'biosample' = multiple controls should be matched on the biosample\
-                              default is multi",
-                        choices=["single", "multi", "biosample"], default="multi")
+#    parser.add_argument('--method',
+#                        help="'single' = there is only one replicate in the control, \
+#                              'multi' = one control with same number of replicates as experiment has replicates, \
+#                              'biosample' = multiple controls should be matched on the biosample",
+#                        choices=["single", "multi", "biosample"])
     parser.add_argument('--ignore_runtype',
-                        help="Ignores value of paired-end. Default is False",
+                        help="Ignores value of paired-end. Default is off",
                         default=False,
                         action='store_true')
     parser.add_argument('--infile',
@@ -41,15 +40,16 @@ def getArgs():
     parser.add_argument('--debug',
                         default=False,
                         action='store_true',
-                        help="Print debug messages.  Default is False.")
+                        help="Print debug messages.  Default is off.")
     parser.add_argument('--update',
                         default=False,
                         action='store_true',
-                        help="Let the script PATCH the data.  Default is False")
+                        help="Let the script PATCH the data.  Default is off")
     parser.add_argument('--missing',
                         default=False,
                         action='store_true',
-                        help="only print files that are missing controlled_by")
+                        help="Only print files that are missing controlled_by.\
+                        Default is off")
     args = parser.parse_args()
     return args
 
@@ -76,7 +76,7 @@ class BackFill:
                     if e.get("file_type", "") == "fastq":
                         if not self.MISSING or (self.MISSING and not e.get("controlled_by")):
                                 exp_list.append(e["accession"])
-                temp = {"Experiment": exp_list, "Control": c["accession"]}
+                temp = {"Exp Accession": obj["accession"], "Check type": "Single", "Experiment": exp_list, "Control": c["accession"]}
                 if len(exp_list) > 0:
                     self.data.append(temp)
                 if self.DEBUG:
@@ -125,7 +125,7 @@ class BackFill:
                 for c_key in con_data.keys():
                     if exp_data[e_key] == con_data[c_key]:
                         con_list.append(c_key)
-                temp = {"Experiment": e_key, "Control": con_list}
+                temp = {"Exp Accession": obj["accession"], "Check type": "Multi-runtype ignored", "Experiment": e_key, "Control": con_list}
                 self.data.append(temp)
                 if self.DEBUG:
                     print("experiment files", e_key)
@@ -136,7 +136,7 @@ class BackFill:
                 for e_key in exp_data.keys():
                     if con_data[c_key] == exp_data[e_key]:
                         exp_list.append(e_key)
-                temp = {"Experiment": exp_list, "Control": c_key}
+                temp = {"Exp Accession": obj["accession"], "Check type": "Multi", "Experiment": exp_list, "Control": c_key}
                 if len(exp_list) > 0:
                     self.data.append(temp)
                 if self.DEBUG:
@@ -149,25 +149,26 @@ class BackFill:
         val = True
         for con in obj["possible_controls"]:
             c = encodedcc.get_ENCODE(con["accession"], self.connection, frame="embedded")
-            if len(c.get("replicates", [])) == 0:
-                if self.DEBUG:
-                    print("No replicates found in control {}".format(con["accession"]), file=sys.stderr)
-                val = False
-            else:
+            if c.get("replicates"):
                 for rep in c["replicates"]:
-                    con_bio_acc = rep["library"]["biosample"]["accession"]
-                    con_bio_num = rep["biological_replicate_number"]
-                    if len(c.get("files", [])) == 0:
-                        if self.DEBUG:
-                            print("No files found for control {}".format(con["accession"]), file=sys.stderr)
-                        val = False
-                    else:
+                    if c.get("files"):
+                        con_bio_acc = rep["library"]["biosample"]["accession"]
+                        con_bio_num = rep["biological_replicate_number"]
                         for f in c["files"]:
                             if f.get("file_type", "") == "fastq":
                                 con_file_bio_num = f["biological_replicates"]
                                 if con_bio_num in con_file_bio_num:
                                     con_file_acc = f["accession"]
                                     con_data[con_bio_acc] = con_file_acc
+                    else:
+                        if self.DEBUG:
+                            print("No files found for control {}".format(con["accession"]), file=sys.stderr)
+                        val = False
+            else:
+                if self.DEBUG:
+                    print("No replicates found in control {}".format(con["accession"]), file=sys.stderr)
+                val = False
+
         if val:
             exp_data = {}
             for e in obj["replicates"]:
@@ -183,7 +184,7 @@ class BackFill:
 
             for key in exp_data.keys():
                 if con_data.get(key):
-                    temp = {"Experiment": exp_data[key], "Control": con_data[key]}
+                    temp = {"Exp Accession": obj["accession"], "Check type": "Biosample", "Experiment": exp_data[key], "Control": con_data[key]}
                     self.data.append(temp)
                     if self.DEBUG:
                         print("Biosample {}: files {}".format(key, temp))
@@ -211,27 +212,71 @@ def main():
                 print("Experiment", acc)
             obj = encodedcc.get_ENCODE(acc, connection, frame="embedded")
             isValid = True
-            check = ["possible_controls", "replicates", "files"]
+            check = ["replicates", "files"]
             for c in check:
-                if len(obj.get(c, [])) == 0:
+                if not obj.get(c):
                     if args.debug:
                         print("Missing {} for {}".format(c, acc), file=sys.stderr)
                     isValid = False
+            if obj.get("possible_controls"):
+                for p in obj["possible_controls"]:
+                    for c in check:
+                        if not obj.get(c):
+                            if args.debug:
+                                print("Missing {} for {}".format(c, p["accession"]), file=sys.stderr)
+                            isValid = False
+            else:
+                isValid = False
+                if args.debug:
+                    print("Missing possible_controls for {}".format(acc), file=sys.stderr)
             if isValid:
                 b = BackFill(connection, dataList, debug=args.debug, missing=args.missing)
-                if args.method == "single":
-                    b.single_rep(obj)
-                elif args.method == "multi":
-                    b.multi_rep(obj, args.ignore_runtype)
-                elif args.method == "biosample":
-                    b.multi_control(obj)
+                exp_rep = len(obj["replicates"])
+                exp_con = len(obj["possible_controls"])
+                if exp_con == 1:
+                    # one possible control
+                    con_rep = len(obj["possible_controls"][0]["replicates"])
+                    if con_rep == exp_rep:
+                        # same number experiment replicates as control replicates
+                        # method is multi
+                        b.multi_rep(obj, args.ignore_runtype)
+                        print("MULTI REP {}".format(acc))
+                    elif con_rep == 1:
+                        # one control replicate and multiple experiment replicates
+                        # method is single
+                        b.single_rep(obj)
+                        print("SINGLE REP {}".format(acc))
+                    else:
+                        print("Experiment {} does not fit any of the current patterns!".format(acc))
+                elif exp_con > 1:
+                    # more than one possible control
+                    con_reps = 0
+                    for con in obj["possible_controls"]:
+                        if len(con["replicates"]) == 1:
+                            con_reps += 1
+                    if con_reps == exp_rep:
+                        # same number of controls with one replicate as number of experiment replicates
+                        # method is biosample
+                        b.multi_control(obj)
+                        print("MULTI CONTROL {}".format(acc))
+                    else:
+                        print("Experiment {} does not fit any of the current patterns!".format(acc))
                 else:
-                    print("ERROR: unrecognized method:", args.method, file=sys.stderr)
-                    sys.exit(1)
+                    print("Experiment {} does not fit any of the current patterns!".format(acc))
+
+#                if args.method == "single":
+#                    b.single_rep(obj)
+#                elif args.method == "multi":
+#                    b.multi_rep(obj, args.ignore_runtype)
+#                elif args.method == "biosample":
+#                    b.multi_control(obj)
+#                else:
+#                    print("ERROR: unrecognized method:", args.method, file=sys.stderr)
+#                    sys.exit(1)
         if len(dataList) > 0:
-            print("Control Files\tExperiment Files")
+            print("Experiment Accession\tCheck Type\tControl Files\tExperiment Files")
             for d in dataList:
-                print("{}\t{}".format(d["Control"], d["Experiment"]))
+                print("{}\t{}\t{}\t{}".format(d["Exp Accession"], d["Check type"], d["Control"], d["Experiment"]))
 
 if __name__ == '__main__':
         main()
