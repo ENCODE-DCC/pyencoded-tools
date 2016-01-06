7c7dddbfc2a31a1e8243a4d433c222feac9f65bd
Marissa Melen <marissa@heatseq.stanford.edu>
2016-01-06 09:28:45 -0800

    if removing selected skip all data checking

--- encodedcc.py
+++ encodedcc.py
@@ -437,54 +437,54 @@ def patch_set(args, connection):
         if not accession:
             print("No identifier found in headers!  Cannot PATCH data")
             sys.exit(1)
-        patch_data = {}
-        if args.flowcell:
-            # if flowcell is picked search for flowcell details
-            flow = ["flowcell", "lane", "machine", "barcode"]
-            cell = {}
-            for f in flow:
-                if temp_data.get(f):
-                    cell[f] = temp_data[f]
-                    temp_data.pop(f)
-            temp_data["flowcell_details:list"] = cell
-        for key in temp_data.keys():
-            k = key.split(":")
-            if len(k) > 1:
-                if k[1] == "int":
-                    patch_data[k[0]] = int(temp_data[key])
-                elif k[1] == "list":
-                    if type(temp_data[key]) == dict:
-                        l = [temp_data[key]]
-                    else:
-                        l = temp_data[key].strip("[]").split(",")
-                        l = [x.replace(" ", "") for x in l]
-                    if args.overwrite:
-                        patch_data[k[0]] = l
-                    else:
-                        append_list = get_ENCODE(accession, connection).get(k[0], [])
-                        patch_data[k[0]] = l + append_list
-            else:
-                patch_data[k[0]] = temp_data[key]
         accession = quote(accession)
         full_data = get_ENCODE(accession, connection, frame="edit")
-        old_data = {}
-        for key in patch_data.keys():
-            old_data[key] = full_data.get(key)
         if args.remove:
+            put_dict = full_data
+            for key in temp_data.keys():
+                put_dict.pop(key, None)
+                print("OBJECT:", accession)
+                print("Removing value:", key)
             if args.update:
-                put_dict = full_data
-                for key in patch_data.keys():
-                    put_dict.pop(key, None)
                 replace_ENCODE(accession, connection, put_dict)
-            print("OBJECT:", accession)
-            print("Removing values", str(patch_data.keys()))
         else:
-            if args.update:
-                patch_ENCODE(accession, connection, patch_data)
-            print("OBJECT:", accession)
-            for key in patch_data.keys():
-                print("OLD DATA:", key, old_data[key])
-                print("NEW DATA:", key, patch_data[key])
+            patch_data = {}
+            if args.flowcell:
+                # if flowcell is picked search for flowcell details
+                flow = ["flowcell", "lane", "machine", "barcode"]
+                cell = {}
+                for f in flow:
+                    if temp_data.get(f):
+                        cell[f] = temp_data[f]
+                        temp_data.pop(f)
+                temp_data["flowcell_details:list"] = cell
+            for key in temp_data.keys():
+                k = key.split(":")
+                if len(k) > 1:
+                    if k[1] == "int":
+                        patch_data[k[0]] = int(temp_data[key])
+                    elif k[1] == "list":
+                        if type(temp_data[key]) == dict:
+                            l = [temp_data[key]]
+                        else:
+                            l = temp_data[key].strip("[]").split(",")
+                            l = [x.replace(" ", "") for x in l]
+                        if args.overwrite:
+                            patch_data[k[0]] = l
+                        else:
+                            append_list = get_ENCODE(accession, connection).get(k[0], [])
+                            patch_data[k[0]] = l + append_list
+                else:
+                    patch_data[k[0]] = temp_data[key]
+                old_data = {}
+                for key in patch_data.keys():
+                    old_data[key] = full_data.get(key)
+                print("OBJECT:", accession)
+                for key in patch_data.keys():
+                    print("OLD DATA:", key, old_data[key])
+                    print("NEW DATA:", key, patch_data[key])
+                if args.update:
+                    patch_ENCODE(accession, connection, patch_data)
 
 
 def fastq_read(connection, uri=None, filename=None, reads=1):
