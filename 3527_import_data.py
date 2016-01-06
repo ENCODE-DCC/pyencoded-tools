c21a8e8597036f4186596d976c7652eac628ec41
Marissa Melen <marissa@heatseq.stanford.edu>
2016-01-06 14:26:55 -0800

    testing data

---
+++ 3527_import_data.py
@@ -0,0 +1,54 @@
+import argparse
+import os.path
+import encodedcc
+import csv
+import zipfile
+
+EPILOG = '''
+For more details:
+
+        %(prog)s --help
+'''
+
+
+def getArgs():
+    parser = argparse.ArgumentParser(
+        description=__doc__, epilog=EPILOG,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+
+    parser.add_argument('path',
+                        help="input zip file/directory of excel/csv/tsv sheets.")
+    parser.add_argument('--key',
+                        default='default',
+                        help="The keypair identifier from the keyfile.  \
+                        Default is --key=default")
+    parser.add_argument('--keyfile',
+                        default=os.path.expanduser("~/keypairs.json"),
+                        help="The keypair file.  Default is --keyfile=%s" % (os.path.expanduser("~/keypairs.json")))
+    parser.add_argument('--debug',
+                        default=False,
+                        action='store_true',
+                        help="Print debug messages.  Default is False.")
+    parser.add_argument('--update',
+                        default=False,
+                        action='store_true',
+                        help="Let the script PATCH the data.  Default is False")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+
+    args = getArgs()
+    key = encodedcc.ENC_Key(args.keyfile, args.key)
+    connection = encodedcc.ENC_Connection(key)
+
+    with open(args.path) as csvfile:
+        reader = csv.DictReader(csvfile, dialect='excel')
+        for row in reader:
+            print(row)
+
+
+if __name__ == '__main__':
+        main()
