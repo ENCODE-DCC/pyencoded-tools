55c278d01bef4c0b62e2ce312d12cfcb316f1847
Marissa Melen <marissa@heatseq.stanford.edu>
2016-01-08 13:07:43 -0800

    reads in excel files, posting not enabled yet

--- 3527_import_data.py
+++ 3527_import_data.py
@@ -1,8 +1,9 @@
 import argparse
 import os.path
 import encodedcc
-import csv
-import zipfile
+import xlrd
+import datetime
+import sys
 
 EPILOG = '''
 For more details:
@@ -17,8 +18,10 @@ def getArgs():
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
 
-    parser.add_argument('path',
-                        help="input zip file/directory of excel/csv/tsv sheets.")
+    parser.add_argument('infile',
+                        help="the datafile containing object data to import")
+    parser.add_argument('--type',
+                        help="the type of the objects to import")
     parser.add_argument('--key',
                         default='default',
                         help="The keypair identifier from the keyfile.  \
@@ -38,17 +41,84 @@ def getArgs():
     return args
 
 
+def reader(filename, sheetname=None):
+    """ Read named sheet or first and only sheet from xlsx file
+    """
+    book = xlrd.open_workbook(filename)
+    if sheetname is None:
+        sheet, = book.sheets()
+    else:
+        try:
+            sheet = book.sheet_by_name(sheetname)
+        except xlrd.XLRDError:
+            return
+
+    datemode = sheet.book.datemode
+    for index in range(sheet.nrows):
+        yield [cell_value(cell, datemode) for cell in sheet.row(index)]
+
+
+def cell_value(cell, datemode):
+    ctype = cell.ctype
+    value = cell.value
+
+    if ctype == xlrd.XL_CELL_ERROR:
+        raise ValueError(repr(cell), 'cell error')
+
+    elif ctype == xlrd.XL_CELL_BOOLEAN:
+        return str(value).upper()
+
+    elif ctype == xlrd.XL_CELL_NUMBER:
+        if value.is_integer():
+            value = int(value)
+        return str(value)
+
+    elif ctype == xlrd.XL_CELL_DATE:
+        value = xlrd.xldate_as_tuple(value, datemode)
+        if value[3:] == (0, 0, 0):
+            return datetime.date(*value[:3]).isoformat()
+        else:
+            return datetime.datetime(*value).isoformat()
+
+    elif ctype in (xlrd.XL_CELL_TEXT, xlrd.XL_CELL_EMPTY, xlrd.XL_CELL_BLANK):
+        return value
+
+    raise ValueError(repr(cell), 'unknown cell type')
+
+
+def excel_reader(datafile, sheet, update, connection):
+    row = reader(datafile, sheetname=sheet)
+    print(sheet)
+    keys = next(row)  # grab the first row of headers
+    print("headers", keys)
+    for values in row:
+        post_json = dict(zip(keys, values))
+        print(post_json)
+        if update:
+            print("Updating data!")
+            #encodedcc.new_ENCODE(connection, sheet, post_json)
+
+
 def main():
 
     args = getArgs()
     key = encodedcc.ENC_Key(args.keyfile, args.key)
     connection = encodedcc.ENC_Connection(key)
-
-    with open(args.path) as csvfile:
-        reader = csv.DictReader(csvfile, dialect='excel')
-        for row in reader:
-            print(row)
-
+    if args.type:
+        names = [args.type]
+    else:
+        book = xlrd.open_workbook(args.infile)
+        names = book.sheet_names()
+    profiles = encodedcc.get_ENCODE("/profiles/", connection)
+    supported_collections = list(profiles.keys())
+    supported_collections.append("Sheet1")
+    supported_collections = [s.lower() for s in supported_collections]
+#    supported_collections = [s.lower() for s in list(profiles.keys())]
+    for n in names:
+        if n.lower() in supported_collections:
+            excel_reader(args.infile, n, args.update, connection)
+        else:
+            print("Sheet name '{}' not part of supported object types!".format(n), file=sys.stderr)
 
 if __name__ == '__main__':
         main()
