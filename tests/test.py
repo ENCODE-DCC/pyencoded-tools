d93d61160d2879357c0f1c2ecc3e78b4e86396d8
Marissa Melen <marissa@heatseq.stanford.edu>
2016-02-11 16:49:45 -0800

    more tests!

--- tests/test.py
+++ tests/test.py
@@ -1,22 +1,62 @@
 import pytest
 import encodedcc
 import os.path
+# so I don't forget
+# py.test test.py
+# py.test with -v for verbose, or -m key/connection/etc to use the marked tests
 
 def test_nothing():
     assert(1)
 
+
+@pytest.mark.key
 def test_key():
     key = encodedcc.ENC_Key(os.path.expanduser("~/keypairs.json"), "default")
     assert(key)
 
+
+@pytest.mark.key
 def test_key_server():
     key = encodedcc.ENC_Key(os.path.expanduser("~/keypairs.json"), "default")
     assert(key.server)
 
+
+@pytest.mark.key
 def test_key_aithid():
     key = encodedcc.ENC_Key(os.path.expanduser("~/keypairs.json"), "default")
     assert(key.authid)
 
+
+@pytest.mark.key
 def test_key_authpw():
     key = encodedcc.ENC_Key(os.path.expanduser("~/keypairs.json"), "default")
     assert(key.authpw)
+
+
+@pytest.mark.connection
+def test_connection():
+    key = encodedcc.ENC_Key(os.path.expanduser("~/keypairs.json"), "default")
+    connection = encodedcc.ENC_Connection(key)
+    assert(connection)
+
+
+@pytest.mark.connection
+def test_connection_key():
+    key = encodedcc.ENC_Key(os.path.expanduser("~/keypairs.json"), "default")
+    connection = encodedcc.ENC_Connection(key)
+    assert(connection.auth)
+
+
+@pytest.mark.connection
+def test_connection_server():
+    key = encodedcc.ENC_Key(os.path.expanduser("~/keypairs.json"), "default")
+    connection = encodedcc.ENC_Connection(key)
+    assert(connection.server)
+
+
+@pytest.mark.get
+def test_get():
+    key = encodedcc.ENC_Key(os.path.expanduser("~/keypairs.json"), "default")
+    connection = encodedcc.ENC_Connection(key)
+    result = encodedcc.get_ENCODE("/profiles/", connection)
+    assert(result)
