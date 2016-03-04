0e2c1aef5e926e1feb7f07b71549cfbb7dcd0db8
Marissa Melen <marissa@heatseq.stanford.edu>
2016-03-04 10:06:41 -0800

    adding in attachment stuff

--- ENCODE_import_data.py
+++ ENCODE_import_data.py
@@ -6,6 +6,12 @@ import encodedcc
 import xlrd
 import datetime
 import sys
+import mimetypes
+from PIL import Image
+from base64 import b64encode
+import magic  # install me with 'pip install python-magic'
+# https://github.com/ahupp/python-magic
+# this is the site for python-magic in case we need it
 
 EPILOG = '''
 For more details:
@@ -46,6 +52,47 @@ def getArgs():
                         and will only PATCH with user override")
     args = parser.parse_args()
     return args
+
+
+def attachment(path):
+    """ Create an attachment upload object from a filename
+    Embeds the attachment as a data url.
+    """
+
+    filename = os.path.basename(path)
+    mime_type, encoding = mimetypes.guess_type(path)
+    major, minor = mime_type.split('/')
+    detected_type = magic.from_file(path, mime=True).decode('ascii')
+
+    # XXX This validation logic should move server-side.
+    if not (detected_type == mime_type or
+            detected_type == 'text/plain' and major == 'text'):
+        raise ValueError('Wrong extension for %s: %s' % (detected_type, filename))
+
+    with open(path, 'rb') as stream:
+        attach = {
+            'download': filename,
+            'type': mime_type,
+            'href': 'data:%s;base64,%s' % (mime_type, b64encode(stream.read()).decode('ascii'))
+        }
+
+        if mime_type in ('application/pdf', 'text/plain', 'text/tab-separated-values', 'text/html'):
+            # XXX Should use chardet to detect charset for text files here.
+            return attach
+
+        if major == 'image' and minor in ('png', 'jpeg', 'gif', 'tiff'):
+            # XXX we should just convert our tiffs to pngs
+            stream.seek(0, 0)
+            im = Image.open(stream)
+            im.verify()
+            if im.format != minor.upper():
+                msg = "Image file format %r does not match extension for %s"
+                raise ValueError(msg % (im.format, filename))
+
+            attach['width'], attach['height'] = im.size
+            return attach
+
+    raise ValueError("Unknown file type for %s" % filename)
 
 
 def reader(filename, sheetname=None):
