32374f62646f83f1e7753c9f7af1f6edfdaf078f
Cricket Sloan <cricketsloan@gmail.com>
2016-09-13 13:38:09 -0700

    Adding Chip-seq, cleaning up the rows and columns to be passed for RNA-seq

--- ENCODE_processing_summary.py
+++ ENCODE_processing_summary.py
@@ -5,6 +5,7 @@
 import os.path
 import argparse
 import encodedcc
+import collections
 from encodedcc import get_ENCODE
 
 EPILOG = '''
@@ -36,7 +37,7 @@ def getArgs():
     return args
 
 
-def make_rna_report(connection):
+def make_rna_report(connection, columns, rows):
 
     basic_query = 'search/?type=Experiment'
 
@@ -55,34 +56,6 @@ def make_rna_report(connection):
         'microRNA counts': '&assay_title=microRNA+counts',
         }
 
-    total_query = '&status=released&status=submitted&status=started&status=proposed&status=ready+for+review'
-    released_query = '&status=released'
-    proposed_query = '&status=proposed'
-    unreleased_query = '&status=submitted&status=ready+for+review&status=started'
-    concerns_query = '&internal_status=no+available+pipeline&internal_status=requires+lab+review&internal_status=unrunnable&status!=deleted&status!=revoked'
-    grch38_query = '&assembly=GRCh38'
-    hg19_query = '&files.genome_annotation=V19'
-    mm10_query = '&assembly=mm10'
-    uniform_query = '&files.lab.name=encode-processing-pipeline'
-    audits_query = '&audit.NOT_COMPLIANT.category=missing+controlled_by&audit.NOT_COMPLIANT.category=insufficient+read+depth&audit.NOT_COMPLIANT.category=missing+documents&audit.NOT_COMPLIANT.category=unreplicated+experiment&assay_slims=Transcription&audit.NOT_COMPLIANT.category=missing+possible_controls&audit.NOT_COMPLIANT.category=missing+spikeins&audit.NOT_COMPLIANT.category=missing+RNA+fragment+size'
-    processing_query = '&internal_status=pipeline+ready&internal_status=processing'
-
-    rows = {
-        'Total': total_query,
-        'Released': released_query,
-        'Released with issues': released_query+audits_query,
-        'Unreleased': unreleased_query,
-        'Proposed': proposed_query,
-        'Processed on GRCh38': total_query + grch38_query + uniform_query,
-        'Submitted on GRCh38': total_query + grch38_query,
-        'Uniformely Processed on hg19': total_query + hg19_query + uniform_query,
-        'Submitted on hg19': total_query + hg19_query,
-        'Processed on mm10': total_query + mm10_query + uniform_query,
-        'Submitted on mm10': total_query + mm10_query,
-        'Cannot be currently processed': concerns_query,
-        'In processing queue': processing_query
-    }
-
     labels = [
         'Total',
         'Released',
@@ -96,7 +69,6 @@ def make_rna_report(connection):
         'In processing queue'
     ]
 
-
     micro_labels = [
         'Total',
         'Released',
@@ -108,20 +80,7 @@ def make_rna_report(connection):
         'Submitted on mm10'
     ]
 
-
-    columns = {
-        'ENCODE3': '&award.rfa=ENCODE3',
-        'ENCODE2': '&award.rfa=ENCODE2',
-        'ENCODE2-Mouse': '&award.rfa=ENCODE2-Mouse',
-        'Total': '&award.rfa=ENCODE3&award.rfa=ENCODE2&award.rfa=ENCODE2-Mouse'
-        }
-
-    headers = [
-        'ENCODE3',
-        'ENCODE2',
-        'ENCODE2-Mouse',
-        'Total'
-        ]
+    headers = list(columns.keys())
 
     for assay in assays.keys():
         print (assay, '--------')
@@ -180,143 +139,87 @@ def make_antibody_detail(graph):
                     ]
 
 
-
-def make_chip_report(connection):
+def make_chip_report(connection, columns):
 
     basic_query = 'search/?type=Experiment&award.rfa=ENCODE3&assay_term_name=ChIP-seq'
 
-    rows = {
-        'controls': '&target.investigated_as=control',
-        'experiments': '&target.investigated_as%21=control',
-        'total': ''
-        }
+    catagories = collections.OrderedDict([
+        ('controls', '&target.investigated_as=control'),
+        ('histone mods', '&target.investigated_as=histone'),
+        ('other targets', '&target.investigated_as%21=control&target.investigated_as=histone')
+        ])
 
+    total_query = '&status=released&status=submitted&status=started&status=proposed&status=ready+for+review'
     released_query = '&status=released'
-    no_concerns_query = '&internal_status%21=requires+lab+review&internal_status%21=unrunnable'
+    proposed_query = '&status=proposed'
+    unreleased_query = '&status=submitted&status=ready+for+review&status=started'
+    concerns_query = '&internal_status=no+available+pipeline&internal_status=requires+lab+review&internal_status=unrunnable'
+    grch38_query = '&assembly=GRCh38&'
+    hg19_query = '&assembly=hg19'
+    mm10_query = '&assembly=mm10'
+    uniform_query = '&files.lab.name=encode-processing-pipeline'
+    audits_query = '&audit.NOT_COMPLIANT.category=missing+controlled_by&audit.NOT_COMPLIANT.category=insufficient+read+depth&audit.NOT_COMPLIANT.category=missing+documents&audit.NOT_COMPLIANT.category=unreplicated+experiment&assay_slims=Transcription&audit.NOT_COMPLIANT.category=missing+possible_controls&audit.NOT_COMPLIANT.category=missing+spikeins&audit.NOT_COMPLIANT.category=missing+RNA+fragment+size'   
     concerns_query = '&internal_status=requires+lab+review&internal_status=unrunnable'
-    read_depth_query = '&audit.NOT_COMPLIANT.category=insufficient+read+depth'
-    read_depth_query_3 = '&audit.WARNING.category=low+read+depth'
-    complexity_query = '&audit.NOT_COMPLIANT.category=insufficient+library+complexity'
-    read_length_query = '&files.read_length=271272&files.read_length=657265&files.read_length=25&files.read_length=31&files.read_length=30'
-    antibody_query = '&audit.NOT_COMPLIANT.category=not+eligible+antibody'
-    concordance_query = '&searchTerm=IDR%3Afail'  #'&searchTerm=IDR%3Afail'
-    unrunnable_query = '&internal_status=unrunnable'
-    controls_query = ''
-    submitted_query = '&status=submitted'
-    pipeline_query = '&files.analysis_step_version.analysis_step.pipelines.title=Transcription+factor+ChIP-seq'
+    antibody_query = '&audit.NOT_COMPLIANT.category=not+eligible+antibody'  
     processing_query = '&internal_status=pipeline+ready&internal_status=processing'
-    unreleased_query = '&status=submitted&status=in+progress&status=ready+for+review&status=release+ready&status=started'
-    unreplicated_query = '&replication_type=unreplicated'
-    not_pipeline_query = '&files.analysis_step_version.analysis_step.pipelines.title%21=Transcription+factor+ChIP-seq'
-    no_peaks_query = '&files.file_type!=bigBed+narrowPeak'
-    proposed_query = '&status=proposed&status=preliminary'
 
     queries = {
-        'Total': '&status%21=replaced&status%21=deleted&status%21=revoked',
-        'Proposed': proposed_query,
+        'Total': total_query,
         'Released': released_query,
-        'Released in pipeline': released_query+processing_query,
-        'Released cannot run in pipeline': released_query+unrunnable_query,
-        'Released with no known issues': released_query+no_concerns_query,
-        'Released with issues': released_query+concerns_query,
-        'Released with failing ENCODE2 read-depth': released_query+read_depth_query,
-        'Released with failing ENCODE3 read-depth': released_query+read_depth_query_3,
-        'Released with complexity issues': released_query+complexity_query,
-        'Released with concordance issues': released_query+concordance_query,
-        'Released with antibody issues': released_query+antibody_query,
-        'Released with read-length issues': released_query+read_length_query,
-        'Released unreplicated': released_query+unreplicated_query,
-        'Released missing pipeline': released_query+not_pipeline_query,
+        'Released with antibody issues': released_query + audits_query,
+        'Released with other issues': released_query + antibody_query,
         'Unreleased': unreleased_query,
-        'Unreleased with no known issues': unreleased_query+no_concerns_query,
-        'Unreleased with issues': unreleased_query+concerns_query,
-        'Unreleased cannot run in pipeline': unreleased_query+unrunnable_query,
-        'Unreleased in pipeline': unreleased_query+processing_query,
-        'Unreleased with partial pipeline': unreleased_query+pipeline_query+no_peaks_query,
-        'Unreleased with failing ENCODE2 read-depth': unreleased_query+read_depth_query,
-        'Unreleased with failing ENCODE3 read-depth': unreleased_query+read_depth_query_3,
-        'Unreleased with complexity issues': unreleased_query+complexity_query,
-        'Unreleased with concordance issues': unreleased_query+concordance_query,
-        'Unreleased with antibody issues': unreleased_query+antibody_query,
-        'Unreleased with read-length issues': unreleased_query+read_length_query,
-        'Unreleased unreplicated': unreleased_query+unreplicated_query,
+        'Proposed': proposed_query,
+        'Processed on GRCh38': total_query + grch38_query + uniform_query,
+        'Submitted on GRCh38': total_query + grch38_query,
+        'Uniformly Processed on hg19': total_query + hg19_query + uniform_query,
+        'Processed on mm10': total_query + mm10_query + uniform_query,
+        'Released in pipeline': released_query+processing_query,
+        'Cannot be currently processed': concerns_query,
+        'In processing queue': processing_query
     }
 
-    headers = [
+    rows = [
         'Total',
-        'Proposed',
         'Released',
-        'Released cannot run in pipeline',
-        'Released in pipeline',
-        # 'Released with no known issues',
-        'Released with failing ENCODE2 read-depth',
-        'Released with failing ENCODE3 read-depth',
-        # 'Released with complexity issues',
-        'Released with concordance issues',
         'Released with antibody issues',
-        'Released with read-length issues',
-        'Released unreplicated',
-        'Released missing pipeline',
-        'Released with issues',
+        'Released with other issues',
         'Unreleased',
-        'Unreleased with issues',
-        'Unreleased cannot run in pipeline',
-        'Unreleased in pipeline',
-        'Unreleased with partial pipeline',
-        'Unreleased with failing ENCODE2 read-depth',
-        'Unreleased with failing ENCODE3 read-depth',
-        'Unreleased with complexity issues',
-        'Unreleased with concordance issues',
-        'Unreleased with antibody issues',
-        'Unreleased with read-length issues',
-        'Unreleased unreplicated',
+        'Proposed',
+        'Processed on GRCh38',
+        'Uniformly Processed on hg19',
+        'Processed on mm10',
+        'Cannot be currently processed',
+        'In processing queue'
         ]
 
-    for lab in labs.keys():
-        print (lab, '--------------------------------------')
+    headers = list(columns.keys())
+
+    for catagory in catagories.keys():
+        print (catagory, '--------------------------------------')
         print ('\t'.join([''] + headers))
 
         matrix = {}
 
-        for row in rows.keys():
+        for row in rows:
 
             matrix[row] = [row]
 
             for col in headers:
-                query = basic_query+labs[lab]+rows[row]+queries[col]
+                query = basic_query+catagories[catagory]+queries[row]+columns[col]
                 res = get_ENCODE(query, connection, frame='embedded')
                 link = connection.server + query
                 total = res['total']
-
-                #if col == 'Released with antibody issues':
-                #    make_antibody_detail(res['@graph'])
-                if col in [
-                    'XUnreleased with concordance issues',
-                    'XReleased with concordance issues',
-                    #'Unreleased with complexity issues',
-                    #'Released with complexity issues',
-                ]:
-                    total = 'no audit'
-                if col == 'Unreleased with metadata issues':
-                    total = make_errors_detail(res['facets'], link)
-
                 func = '=HYPERLINK(' + '"' + link + '",' + repr(total) + ')'
                 matrix[row].append(func)
             print ('\t'.join(matrix[row]))
         print (' ')
         print (' ')
 
+
 def make_dna_report(connection):
 
     basic_query = 'search/?type=Experiment&award.project=ENCODE'
-
-    labs = {
-        'John Stamatoyannopoulos': '&lab.title=John+Stamatoyannopoulos%2C+UW',
-        'Barbara Wold': '&lab.title=Barbara+Wold%2C+Caltech',
-        'Ross Hardison': '&lab.title=Ross+Hardison%2C+PennState',
-        'Michael Snyder': '&lab.title=Michael+Snyder%2C+Stanford',
-        'Gregory Crawford': '&lab.title=Gregory+Crawford%2C+Duke',
-        }
 
     assays = {
         'DNase': '&assay_title=DNase-seq',
@@ -365,7 +268,6 @@ def make_dna_report(connection):
         'In processing queue'
     ]
 
-
     columns = {
         'ENCODE3': '&award.rfa=ENCODE3',
         'ENCODE2': '&award.rfa=ENCODE2',
@@ -406,10 +308,63 @@ def main():
     args = getArgs()
     key = encodedcc.ENC_Key(args.keyfile, args.key)
     connection = encodedcc.ENC_Connection(key)
+
+    # ----------- QUERIES ----------------------------------------------------
+    unreplicated_query = '&replication_type=unreplicated'
+    not_pipeline_query = '&files.analysis_step_version.analysis_step.pipelines.title%21=Transcription+factor+ChIP-seq'
+    no_peaks_query = '&files.file_type!=bigBed+narrowPeak'
+    concordance_query = '&searchTerm=IDR%3Afail'  #'&searchTerm=IDR%3Afail'
+    unrunnable_query = '&internal_status=unrunnable'
+    pipeline_query = '&files.analysis_step_version.analysis_step.pipelines.title=Transcription+factor+ChIP-seq'
+    read_depth_query = '&audit.NOT_COMPLIANT.category=insufficient+read+depth'
+    read_depth_query_3 = '&audit.WARNING.category=low+read+depth'
+    complexity_query = '&audit.NOT_COMPLIANT.category=insufficient+library+complexity'
+    read_length_query = '&files.read_length=271272&files.read_length=657265&files.read_length=25&files.read_length=31&files.read_length=30'
+    no_concerns_query = '&internal_status%21=requires+lab+review&internal_status%21=unrunnable'
+
+    human_query = '&replicates.library.biosample.donor.organism.scientific_name=Homo+sapiens'
+    mouse_query = '&replicates.library.biosample.donor.organism.scientific_name=Mus+musculus'
+    ENCODE2_query = '&award.rfa=ENCODE2&award.rfa=ENCODE2-Mouse'
+    total_query = '&status=released&status=submitted&status=started&status=proposed&status=ready+for+review'
+    released_query = '&status=released'
+    proposed_query = '&status=proposed'
+    unreleased_query = '&status=submitted&status=ready+for+review&status=started'
+    concerns_query = '&internal_status=no+available+pipeline&internal_status=requires+lab+review&internal_status=unrunnable&status!=deleted&status!=revoked'
+    grch38_query = '&assembly=GRCh38'
+    hg19_query = '&files.genome_annotation=V19'
+    mm10_query = '&assembly=mm10'
+    uniform_query = '&files.lab.name=encode-processing-pipeline'
+    audits_query = '&audit.NOT_COMPLIANT.category=missing+controlled_by&audit.NOT_COMPLIANT.category=insufficient+read+depth&audit.NOT_COMPLIANT.category=missing+documents&audit.NOT_COMPLIANT.category=unreplicated+experiment&assay_slims=Transcription&audit.NOT_COMPLIANT.category=missing+possible_controls&audit.NOT_COMPLIANT.category=missing+spikeins&audit.NOT_COMPLIANT.category=missing+RNA+fragment+size'
+    processing_query = '&internal_status=pipeline+ready&internal_status=processing'
+
+    rows = {
+        'Total': total_query,
+        'Released': released_query,
+        'Released with issues': released_query+audits_query,
+        'Unreleased': unreleased_query,
+        'Proposed': proposed_query,
+        'Processed on GRCh38': total_query + grch38_query + uniform_query,
+        'Submitted on GRCh38': total_query + grch38_query,
+        'Uniformly Processed on hg19': total_query + hg19_query + uniform_query,
+        'Submitted on hg19': total_query + hg19_query,
+        'Processed on mm10': total_query + mm10_query + uniform_query,
+        'Submitted on mm10': total_query + mm10_query,
+        'Cannot be currently processed': concerns_query,
+        'In processing queue': processing_query
+    }
+
+    columns = collections.OrderedDict([
+        ('ENCODE3-human', '&award.rfa=ENCODE3' + human_query),
+        ('ENCODE3-mouse', '&award.rfa=ENCODE3' + mouse_query),
+        ('ENCODE2-human', ENCODE2_query + human_query),
+        ('ENCODE2-mouse', ENCODE2_query + mouse_query),
+        ('Total', '&award.rfa=ENCODE3' + ENCODE2_query)
+        ])
+
     if args.datatype == 'CHIP':
-        make_chip_report(connection)
+        make_chip_report(connection, columns)
     elif args.datatype == 'RNA':
-        make_rna_report(connection)
+        make_rna_report(connection, columns, rows)
     elif args.datatype == 'DNA':
         make_dna_report(connection)
     else:
