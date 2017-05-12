b99f37b9a6312dbf84fb335a35f9ddd847c4a809
Idan Gabdank <gabdank@stanford.edu>
2017-05-12 12:16:46 -0700

    finishing to generate list of experiments for mini-demo

--- ENCODE_mini_demo.py
+++ ENCODE_mini_demo.py
@@ -40,13 +40,17 @@ def main():
     key = encodedcc.ENC_Key(args.keyfile, args.key)
     connection = encodedcc.ENC_Connection(key)
 
-    experiments = encodedcc.get_ENCODE('search/?type=Experiment&assay_term_name=HiC&assay_term_name=eCLIP&assay_term_name=ATAC-seq',
+    experiments = encodedcc.get_ENCODE('search/?type=Experiment&assay_term_name=HiC&assay_term_name=RRBS&assay_term_name=ATAC-seq',
                                        connection)['@graph']
     assay_types = []
+    assay_statuses = []
     for ex in experiments:
         if ex['assay_term_name'] not in assay_types:
             assay_types.append(ex['assay_term_name'])
+        if ex['status'] not in assay_statuses:
+            assay_statuses.append(ex['status'])
 
+    accessions_set = set()
     for assay_type in assay_types:
         print (assay_type)
         experiments = encodedcc.get_ENCODE('search/?type=Experiment&assay_term_name=' + assay_type,
@@ -59,12 +63,67 @@ def main():
                 ex_dict[len(ex['original_files'])].append(ex['accession'])
 
         mone = 0
+
         for file_number in sorted(ex_dict.keys(), reverse=True):
-            print (str(file_number) + '\t' + random.choice(ex_dict[file_number]))
+            print (str(file_number))
+            for i in range(3):
+                accessions_set.add(random.choice(ex_dict[file_number]))
+
             mone += 1
-            if mone == 5:
+            if mone == 4:
                 break
 
+    # at this point we have representatives of different assays
+    for assay_type in assay_types:
+        for status in assay_statuses:
+            experiments = encodedcc.get_ENCODE('search/?type=Experiment&assay_term_name=' + assay_type + '&status=' + status,
+                                               connection)['@graph']
+            if (experiments):
+                print (assay_type + '\t' + status)
+                for i in range(5):
+                    random_experiment = random.choice(experiments)
+                    if status == 'replaced':
+                        replacement = encodedcc.get_ENCODE(random_experiment['accession'],
+                                                           connection)
+                        if replacement:
+                            accessions_set.add(replacement['accession'])
+                    accessions_set.add(random_experiment['accession'])
 
+
+    # at this point we have representatives of different statuses of different assays
+
+    series = encodedcc.get_ENCODE('search/?type=Series',
+                                  connection)['@graph']
+    series_types = []
+    for s in series:
+        if s['@type'][0] not in series_types:
+            series_types.append(s['@type'][0])
+
+    for series_type in series_types:
+        print (series_type)
+        series = encodedcc.get_ENCODE('search/?type=' + series_type,
+                                      connection)['@graph']
+        for i in range(3):
+            accessions_set.add(random.choice(series)['accession'])
+
+    # at this point we have representatives of different series
+    antibodies = encodedcc.get_ENCODE('search/?type=AntibodyLot',
+                                      connection)['@graph']
+    antibody_statuses = []
+    for a in antibodies:
+        if a['status'] not in antibody_statuses:
+            antibody_statuses.append(a['status'])
+    for status in antibody_statuses:
+        print (status)
+        antibodies = encodedcc.get_ENCODE('search/?type=AntibodyLot&status=' + status,
+                                          connection)['@graph']
+        for i in range(20):
+            accessions_set.add(random.choice(antibodies)['accession'])
+
+
+    # at this point we have representatives of different antibodies statuses
+
+
+    print (accessions_set)
 if __name__ == '__main__':
     main()
