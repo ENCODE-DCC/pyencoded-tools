0b7eb60221536e303feda128ac2adc37b807dbb0
Keenan Graham <keenangraham@stanford.edu>
2017-06-24 11:41:27 -0700

    Add comments.

--- ENCODE_orphans.py
+++ ENCODE_orphans.py
@@ -8,11 +8,36 @@ import encodedcc
 
 """
 
+Purpose
+-------
 Find orphaned ENCODE objects.
+
+Example
+-------
+A Biosample with no Libraries means that a Biosample
+was created and then never referred to by a Library.
+The Biosample is orphaned.
+
+Method
+-------
+This script gathers all accession/uuids of child objects
+and all accession/uuids of child objects embedded in parent
+objects and reports the difference.
 
 """
 
-# Define parent-child relationship.
+# Define parent-child relationship:
+# A child is the possible orphan while a parent is any
+# object that could refer to it (as defined in the schema
+# at https://www.encodeproject.org/profiles/). The child
+# can have one parent or multiple parents (e.g. Documents).
+# To define an orphan, add a new object to the 'orphans' list,
+# where 'child' is the possible orphan object, 'child_field'
+# is its identification field (accession/uuid/@id), 'parent'
+# is the parent object (or list of parent objects), and 'parent_field'
+# is the child's identification field (accession/uuid/@id) in
+# the parent (a list is required if there are multiple parents,
+# and the 'parent' list and 'parent_field' list must be same length.
 orphans = [{'child': 'Biosample',
             'child_field': 'accession',
             'parent': 'Library',
@@ -115,6 +140,116 @@ orphans = [{'child': 'Biosample',
                              'file_format_specification.@id']}]
 
 
+def get_accessions(item_type, field_name):
+    """Return set of accessions/uuids/@ids given
+    the ENCODE item type and field name defining
+    location of accessions/uuids/@ids.
+    """
+    accessions = []
+    childless_parents = []
+    field_name_split = field_name.split('.')
+    # Grab all objects of that type.
+    url = 'https://www.encodeproject.org/search/'\
+          '?type={}&limit=all&format=json'\
+          '&frame=embedded'.format(item_type)
+    r = requests.get(url, auth=(key.authid, key.authpw))
+    results = r.json()['@graph']
+    print('Total {}: {}'.format(item_type, len(results)))
+    # Process each object one at a time.
+    for result in results:
+        value, values = result, None
+        # Access value in nested fields if required.
+        for name in field_name_split:
+            # Deal with list of objects.
+            if isinstance(value, list):
+                # Recover all values in a list.
+                values = [x.get(name) if isinstance(x, dict)
+                          else x for x in value]
+                # Set singluar value to None because
+                # now have multiple values.
+                value = None
+                # Recovered values so exit loop.
+                break
+            # Otherwise access field name and update value.
+            value = value.get(name, {})
+        # Deal with objects missing field. An empty dict
+        # is returned if field not present.
+        if isinstance(value, dict):
+            childless_parents.append(result.get('accession',
+                                                result.get('uuid')))
+            continue
+        # See if replacement is also orphaned (only for child objects).
+        if ((result.get('status') == 'replaced')
+                and (len(field_name_split) == 1)):
+            url = 'https://www.encodeproject.org/'\
+                  '{}/?format=json'.format(result.get('accession',
+                                                      result.get('uuid',
+                                                                 result.get('@id'))))
+            r = requests.get(url, auth=(key.authid, key.authpw))
+            if (r.status_code == 200):
+                r = r.json()
+                # Set replaced accession to replacement accession.
+                value = r.get('accession',
+                              r.get('uuid',
+                                    r.get('@id')))
+        # Deal with values recovered from a list of objects/strings.
+        if values is not None:
+            # Possible to have empty list.
+            if len(values) == 0:
+                childless_parents.append(result.get('accession',
+                                                    result.get('uuid')))
+            else:
+                # Add recovered values to accessions list.
+                accessions.extend(values)
+            continue
+        # Add recovered value to accessions list.
+        accessions.append(value)
+    if childless_parents:
+        print('Number of {} missing {} field'
+              ' (childless parents): {}'.format(item_type,
+                                                field_name,
+                                                len(childless_parents)))
+    return set(accessions)
+
+
+def find_orphans(i, item):
+    """Return orphans of specified child item"""
+    orphan_data = []
+    # Grab all relationships in 'orphans' list with specified child.
+    relationships = [x for x in orphans if x['child'].lower() == item]
+    for relation in relationships:
+        child, parent, child_field, parent_field = (relation['child'],
+                                                    relation['parent'],
+                                                    relation['child_field'],
+                                                    relation['parent_field'])
+        print('{}. {} not associated with {}'.format((i + 1), child, parent))
+        # Grab set of child accessions.
+        child_accessions = get_accessions(child, child_field)
+        # Grab set of parent accessions.
+        # Deal with multiple parents.
+        if isinstance(parent, list):
+            # Build set from multiple parents.
+            parent_accessions = set()
+            for p, f in zip(parent, parent_field):
+                accessions = get_accessions(p, f)
+                parent_accessions = parent_accessions.union(accessions)
+        else:
+            # If only single parent.
+            parent_accessions = get_accessions(parent, parent_field)
+        # Compare groups.
+        same = child_accessions.intersection(parent_accessions)
+        different = child_accessions.difference(parent_accessions)
+
+        print('Number of {} with {} (families): {}'.format(child,
+                                                           parent,
+                                                           len(same)))
+        print('Number of {} without {} (orphans): {}'.format(child,
+                                                             parent,
+                                                             len(different)))
+        orphan_data.append((child, parent, different))
+    return orphan_data
+
+
 def get_args():
     parser = argparse.ArgumentParser(description='__doc__')
     parser.add_argument('--type',
@@ -133,117 +268,27 @@ def get_args():
     return parser.parse_args()
 
 
-def get_accessions(item_type, field_name):
-    accessions = []
-    childless_parents = []
-    field_name_split = field_name.split('.')
-    url = 'https://www.encodeproject.org/search/'\
-          '?type={}&limit=all&format=json'\
-          '&frame=embedded'.format(item_type)
-    r = requests.get(url, auth=(key.authid, key.authpw))
-    results = r.json()['@graph']
-    print('Total {}: {}'.format(item_type, len(results)))
-    for result in results:
-        value = result
-        values = None
-        for name in field_name_split:
-            # Deal with list of objects.
-            if isinstance(value, list):
-                values = [x.get(name) if isinstance(x, dict)
-                          else x for x in value]
-                value = None
-                continue
-            value = value.get(name, {})
-        # Deal with objects missing field.
-        if isinstance(value, dict):
-            childless_parents.append(result.get('accession',
-                                                result.get('uuid')))
-            continue
-        # See if replacement is also orphaned (only for child objects).
-        if ((result.get('status') == 'replaced')
-                and (len(field_name_split) == 1)):
-            url = 'https://www.encodeproject.org/'\
-                  '{}/?format=json'.format(result.get('accession',
-                                                      result.get('uuid',
-                                                                 result.get('@id'))))
-            r = requests.get(url, auth=(key.authid, key.authpw))
-            if (r.status_code == 200):
-                r = r.json()
-                value = r.get('accession',
-                              r.get('uuid',
-                                    r.get('@id')))
-        # Deal with values recovered from a list of objects/strings.
-        if values is not None:
-            if len(values) == 0:
-                childless_parents.append(result.get('accession',
-                                                    result.get('uuid')))
-            else:
-                accessions.extend(values)
-            continue
-        accessions.append(value)
-    if childless_parents:
-        print('Number of {} missing {} field'
-              ' (childless parents): {}'.format(item_type,
-                                                field_name,
-                                                len(childless_parents)))
-        print('Sample childless parents:')
-        print(*childless_parents[:min([5, len(childless_parents)])], sep='\n')
-    return set(accessions)
-
-
-def find_orphans(i, item):
-    relationships = [x for x in orphans if x['child'].lower() == item]
-    for relation in relationships:
-        child, parent = relation['child'], relation['parent']
-        print('{}. {} not associated with {}'.format((i + 1), child, parent))
-        child_field = relation['child_field']
-        parent_field = relation['parent_field']
-        child_accessions = get_accessions(child, child_field)
-        # Deal with multiple parents.
-        if isinstance(parent, list):
-            parent_accessions = set()
-            for p, f in zip(parent, parent_field):
-                accessions = get_accessions(p, f)
-                parent_accessions = parent_accessions.union(accessions)
-        else:
-            parent_accessions = get_accessions(parent, parent_field)
-        same = child_accessions.intersection(parent_accessions)
-        different = child_accessions.difference(parent_accessions)
-        print('Number of {} with {} (families): {}'.format(child,
-                                                           parent,
-                                                           len(same)))
-        print('Sample families:')
-        sim = iter(same)
-        for i in range(min([5, len(same)])):
-            print(next(sim))
-        print('Number of {} without {} (orphans): {}'.format(child,
-                                                             parent,
-                                                             len(different)))
-        print('Sample orphans:')
-        diff = iter(different)
-        for i in range(min([5, len(different)])):
-            print(next(diff))
-        return (child_accessions, parent_accessions)
-
-
 def main():
     global key
     global args
     args = get_args()
     key = encodedcc.ENC_Key(args.keyfile, args.key)
+    # Build list of ENCODE object types for orphan checking.
     if args.type is None:
+        # Default list.
         item_type = [x['child'].lower() for x in orphans]
         print('Default orphan search:')
     else:
+        # User-defined list.
         input_type = [x.strip().lower() for x in args.type.split(',')]
         item_type = [x for x in input_type
                      if x in [x['child'].lower() for x in orphans]]
+        # Check for invalid --type input from user.
         if len(input_type) != len(item_type):
             raise ValueError("Invalid item type: {}.".format((set(input_type)
                                                               - set(item_type))))
         print('Custom orphan search:')
-    return zip(list(item_type),
-               [find_orphans(i, item) for i, item in enumerate(item_type)])
+    return [find_orphans(i, item) for i, item in enumerate(item_type)]
 
 
 if __name__ == '__main__':
