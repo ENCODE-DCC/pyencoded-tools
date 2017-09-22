2096f649f5a57d311b0245f1ca0ee76db9e03150
Keenan Graham <keenangraham@stanford.edu>
2017-09-21 20:05:56 -0700

    Add wildcards * into recursive JSON crawl and --where keyword for filtering -search_type

--- encode-cli/encode/commands/explore.py
+++ encode-cli/encode/commands/explore.py
@@ -13,6 +13,10 @@ def raise_error(key, field):
         '{} not found'.format(key), bold=True, fg='red'), param_hint=field)
 
 
+def parse_where(where):
+    return '&' + '&'.join([w.strip() for w in where.split(',')])
+
+
 def crawl(*args, **kwargs):
     items_found = []
 
@@ -28,11 +32,15 @@ def crawl(*args, **kwargs):
             return
         current_field = fieldsplit[0]
         if isinstance(data, dict):
+            if current_field == '*':
+                fieldsplit.pop(0)
+                [crawl_object(data[k], fieldsplit) for k in data.keys()]
+                fieldsplit.insert(0, current_field)
+                return
             try:
                 data = data[current_field]
             except KeyError:
                 return
-                raise_error(fieldsplit, '--field')
             fieldsplit.pop(0)
             crawl_object(data, fieldsplit)
             fieldsplit.insert(0, current_field)
@@ -45,6 +53,14 @@ def crawl(*args, **kwargs):
 @click.command()
 @click.argument('encode_object',
                 default='/profiles/')
+@click.option('--search_type',
+              default=None,
+              help='Type of ENCODE object as listed in /profiles/.')
+@click.option('--where',
+              default=None,
+              required=False,
+              help='Filter search_type results using "fieldname1=value2,'
+              ' fieldname2=value2" format.')
 @click.option('--field',
               default=None,
               help='Field to return. List subfields with dot notation: '
@@ -58,13 +74,21 @@ def crawl(*args, **kwargs):
               help='JSON returned as regular object or embedded object.'
               ' Default is object.')
 @click.option('--count/--no-count',
-              default=False)
+              default=False,
+              help='Return count of items.')
 @click.pass_obj
-def explore(ctx, encode_object, field, limit, frame, count):
+def explore(ctx, encode_object, search_type, field, limit, frame, count, where):
     '''
     Explore facets of ENCODE metadata.
     '''
     click.secho('Using server: {}'.format(ctx.connection.server))
+    object_default = '/profiles/'
+    if search_type is not None and encode_object == object_default:
+        encode_object = '/search/?type={}'.format(search_type)
+        if where is not None:
+            encode_object = encode_object + parse_where(where)
+    if where is not None and search_type is None:
+        raise_error('--search_type required for --where', 'where')
     response = encodedcc.get_ENCODE(encode_object,
                                     ctx.connection,
                                     limit=limit,
@@ -81,10 +105,10 @@ def explore(ctx, encode_object, field, limit, frame, count):
     if data:
         click.secho('Found {} {}{}{}'.format(len(data),
                                              encode_object,
-                                             '.' if field else ' ',
+                                             ':' if field else ' ',
                                              field if field else ''),
                     bold=True, fg='green')
-        if isinstance(data[0], dict):
+        if all([isinstance(d, dict) for d in data]):
             click.secho('(Keys)', bold=True)
             keys = list(set([x for d in data for x in d.keys()]))
             key_types = [type(objects_with_key(k, data)[
