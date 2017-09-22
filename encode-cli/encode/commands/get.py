9f28183c7d01b45824214ed39e0c5c810985c363
Keenan Graham <keenangraham@stanford.edu>
2017-09-22 09:58:42 -0700

    Add auth context to regular get command

--- encode-cli/encode/commands/get.py
+++ encode-cli/encode/commands/get.py
@@ -8,13 +8,13 @@ from . import encodedcc
 @click.command()
 @click.argument('encode_object')
 @click.option('--field', default=None, help='Field to return.')
-def get(encode_object, field):
+@click.pass_obj
+def get(ctx, encode_object, field):
     '''
     GETs ENCODE metadata given accession/UUID/alias.
     '''
-    key = encodedcc.ENC_Key(os.path.expanduser('~/keypairs.json'), 'test')
-    connection = encodedcc.ENC_Connection(key)
-    response = encodedcc.get_ENCODE(encode_object, connection)
+    click.secho('Using server: {}'.format(ctx.connection.server))
+    response = encodedcc.get_ENCODE(encode_object, ctx.connection)
     if field is not None:
         response = response[field]
     click.echo(json.dumps(response, indent=4, sort_keys=True))
