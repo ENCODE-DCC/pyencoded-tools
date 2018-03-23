e27ad77ffaaca53da358e3f975c397fc12a558e6
Ben Hitz <hitz@stanford.edu>
2018-03-23 10:17:52 -0700

    add lambda function and utility script

---
+++ awslambda/lambda/reg_gzip_md5.py
@@ -0,0 +1,41 @@
+
+import json
+import urllib.parse
+import boto3
+import re
+from gzip import GzipFile
+from io import BytesIO
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event and show its content type
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    print("Bucket %s Key %s" % (bucket, key))
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
+    tag = re.sub(r'\W', '', response['ETag'])
+    print("ETag: " + tag)
+    gz_body = BytesIO()
+    gz = GzipFile(None, 'wb', 9, gz_body)
+    gz.write(response['Body'].read())
+    gz.close()
+    try:
+        put_res = s3.put_object(Bucket=bucket, Key=key+'.gz', Body=gz_body.getvalue())
+    except Exception as e:
+        print(e)
+        print('Error putting gzipped object {} to bucket {}.'.format(key+'.gz', bucket))
+        raise e
+
+    return {'md5sum': tag, 'submitted_file_name': key+'.gz'}
+
---
+++ awslambda/scripts/gzip_beds.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python3
+
+import boto3
+import json
+import csv
+from botocore.config import Config
+
+config = Config(
+    retries=dict(
+        max_attempts=10
+    ),
+    read_timeout=300
+)
+
+client = boto3.client('lambda', config=config)
+BUCKET = 'regulomedb'
+# FOLDER = 'Histone_Modification/REMC_core_127'
+
+rocket = {
+    "Records":
+        [{
+            "s3": {
+                "bucket":
+                {
+                    "arn": "arn:aws:s3:::" + BUCKET,
+                    "name": BUCKET
+                },
+                "object": {
+                    "key": "Histone_Modification/REMC_core_127/E001_15_coreMarks_mnemonics.bed"}
+                },
+            "awsRegion": "us-west-2"
+        }
+        ]
+}
+
+out = open('reg_remc_bigbed.tsv', 'w')
+with open('reg_remc_hmm_beds.tsv.2', 'r') as bed_list:
+    # https://medium.com/@adds68/parsing-tsv-file-with-csv-in-python-662d6347b0cd
+    reader = csv.DictReader(bed_list, dialect='excel-tab')
+
+    for bed in reader:
+
+        print("{}\t{}".format(bed['dataset'], bed['submitted_file_name']))
+        payload = rocket
+        payload['Records'][0]['s3']['object'] = { "key": bed['submitted_file_name']}
+        # print(json.dumps(payload, sort_keys=True, indent=4))
+        for retry in (1, 2, 3, 4, 5):
+            try:
+                res = client.invoke(FunctionName='reg_gzip_md5', InvocationType='RequestResponse', Payload=json.dumps(payload))
+            except Exception:
+                print("Retry: %s" % retry)
+            finally:
+                break
+
+        returns = res['Payload'].read().decode()
+        print("Status: {} Results {}".format(res['StatusCode'], returns))
+        results = json.loads(returns)
+
+        if (results.get('errorMessage', "")):
+            exit()
+
+        bed['submitted_file_name'] = results['submitted_file_name']
+        bed['md5sum'] = results['md5sum']
+        out.write("\t".join(bed.values())+"\n")
