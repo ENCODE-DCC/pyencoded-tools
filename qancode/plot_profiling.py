eb3d538f79a0729c2938e17e5367a129980017a0
Casey Litton <caseylitton@gmail.com>
2020-05-27 10:30:48 -0700

    WRAN-XXXX Automate profiling

--- qancode/plot_profiling.py
+++ qancode/plot_profiling.py
@@ -15,48 +15,55 @@ sys.path.insert(0, os.path.expanduser('~/pyencoded-tools'))
 from qancode.qancode import QANCODE
 
 
+DEFAULT_ITEM_TYPES = [
+    "/search/?type=Experiment&biosample_ontology.term_name=whole+organism",
+    "/search/?type=Experiment&target.label=H3K4me3",
+    "/search/?type=Experiment&assay_slims=DNA+methylation",
+    "/search/?type=Experiment&status=released",
+    "/matrix/?type=Experiment&status=released",
+    "/search/?searchTerm=hippocampus",
+    "/experiments/ENCSR079YAP/",
+    "/experiments/ENCSR296ASC/",
+    "/publication-data/ENCSR089EOA/",
+]
+
+
 def get_parser():
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("testurl", action='store', help="""URL of the RC or demo.""")
-    parser.add_argument("-i", "--items", action='store', help="""Items to check.""")
+    parser.add_argument("test_tag", action='store', help="Test demo tag, encd-5277 or v101rc1")
+    parser.add_argument("test_url", action='store', help="""URL of the RC or demo.""")
+    parser.add_argument(
+        "compare_url",
+        action='store',
+        default='https://test.encodedcc.org',
+        help="""URL of standard to compare test url."""
+    )
+    parser.add_argument("--dry-run", action='store_true', help="""Just print some stuff.""")
+    parser.add_argument("-n", '--number-of-trials', type=int, default=50, help="Number of runs.")
+    parser.add_argument("-i", "--items", nargs='+', default=DEFAULT_ITEM_TYPES, help="""Items to check.""")
     return parser
 
 
-def main():
-    parser = get_parser()
-    args = parser.parse_args()
-    rc_url = args.testurl
-    item_types = args.items
-    if item_types is not None:
-        item_types = item_types.split(',')
-    else:
-        item_types = [
-            "/search/?type=Experiment&biosample_ontology.term_name=whole+organism",
-            "/search/?type=Experiment&target.label=H3K4me3",
-            "/search/?type=Experiment&assay_slims=DNA+methylation",
-            "/search/?type=Experiment&status=released",
-            "/matrix/?type=Experiment&status=released",
-            "/search/?searchTerm=hippocampus",
-            "/experiments/ENCSR079YAP/",
-            "/experiments/ENCSR296ASC/",
-            "/publication-data/ENCSR089EOA/"
-        ]
-    prod_url = "https://test.encodedcc.org"
+def _setup_data_dir(rc_tag, all_data_dir='~/profiling_output'):
+    import glob
+    all_data_dir = os.path.expanduser(all_data_dir)
+    if not os.path.exists(all_data_dir):
+        os.mkdir(all_data_dir)
+    tag_data_dir = f"{all_data_dir}/{rc_tag}"
+    if not os.path.exists(tag_data_dir):
+        os.mkdir(tag_data_dir)
+    next_run_number=0
+    highest=next_run_number
+    for file_path in glob.glob(f"{tag_data_dir}/[0-9]*_*"):
+        run_number = int(os.path.basename(file_path).split('_', 1)[0])
+        if run_number > next_run_number:
+            next_run_number = run_number
+    next_run_number += 1
+    tag_data_dir = f"{tag_data_dir}/{next_run_number}"
+    return tag_data_dir
+   
 
-    if os.path.exists(os.path.expanduser('~/profiling_output')):
-        if os.path.exists(os.path.expanduser('~/profiling_output_archive')):
-            shutil.rmtree(os.path.expanduser('~/profiling_output_archive'))
-        shutil.copytree(os.path.expanduser('~/profiling_output'), os.path.expanduser('~/profiling_output_archive'))
-        shutil.rmtree(os.path.expanduser('~/profiling_output'))
-    os.mkdir(os.path.expanduser('~/profiling_output'))
-
-    print(datetime.datetime.now())
-    qa = QANCODE(rc_url=rc_url, prod_url=prod_url)
-    num_trials = 50
-    info = qa.check_response_time(
-        item_types=item_types, n=num_trials, output_path=os.path.expanduser('~/profiling_output/check_response_time.txt')
-    )
-
+def _save_pyplt(output_path, prod_url, rc_url, item_types, info):
     prod_url_short = prod_url.split('/')[2]
     test_url_short = rc_url.split('/')[2]
     xticklabels = []
@@ -127,13 +134,40 @@ def main():
     axes[4, 0].set_xticklabels(xticklabels)
     axes[4, 1].set_xticklabels(xticklabels)
 
-    plt.savefig(
-        os.path.expanduser('~/profiling_output/profiling_plot_{}.png'.format(
-            datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S_%f')
-        )),
-        bbox_inches='tight'
+    plt.savefig(output_path, bbox_inches='tight')
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    rc_tag = args.test_tag
+    rc_url = args.test_url
+    prod_url = args.compare_url
+    num_trials = 10 # args.number_of_trials
+    item_types = args.items
+    tag_data_dir = _setup_data_dir(rc_tag)
+    check_response_output_path = f"{tag_data_dir}_check-response-time.txt"
+    check_response_output_plot_path = f"{tag_data_dir}_check-response-time-plot"
+    print(f"Comparing {rc_url} to {prod_url} with {num_trials} trials")
+    print(f"tag_data_dir: {tag_data_dir}")
+    print(f"check_response_output_path: {check_response_output_path}")
+    print(f"check_response_output_plot_path: {check_response_output_plot_path}")
+    qa = QANCODE(rc_url=rc_url, prod_url=prod_url)
+    if args.dry_run:
+        print('Dry Run Over')
+        sys.exit(0)
+    start_time = datetime.datetime.now()
+    print(f"Start Time: {start_time}")
+    info = qa.check_response_time(
+        item_types=item_types, 
+        n=num_trials, 
+        output_path=check_response_output_path,
     )
-    print(datetime.datetime.now())
+    end_time = datetime.datetime.now()
+    end_time_str=end_time.strftime('%Y-%m-%d_%H-%M-%S_%f')
+    _save_pyplt(f"{check_response_output_plot_path}_{end_time_str}.png", prod_url, rc_url, item_types, info)
+    print(f"End Time: {end_time}")
+    print(f"Ran in {end_time - start_time}")
 
 
 if __name__ == '__main__':
