5a4f3a310ba47e026ceea376d864627181eb163a
Casey Litton <caseylitton@gmail.com>
2020-05-30 07:20:09 -0700

    Remove unused

--- ihec/ENCODE_IHEC_data_hub_validation.sh
+++
@@ -1,3 +0,0 @@
-python validateHub.py --json='ENCODE_IHEC_Data_Hub_hg19.json' --verbose --epirr
-python validateHub.py --json='ENCODE_IHEC_Data_Hub_hg38.json' --verbose --epirr
-python validateHub.py --json='ENCODE_IHEC_Data_Hub_mm10.json' --verbose --epirr
