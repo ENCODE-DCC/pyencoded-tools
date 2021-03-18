43078d745b92ca0de10e841fa3782758082b608f
Jessica <jessica.au10@gmail.com>
2021-03-18 13:38:31 -0700

    fixed chip and added bulk rna

---
+++ simple_ENCODE4_pipeline_checks/ENCODE4_bulk_RNA_pipeline_check.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import os
+import sys
+import csv
+import requests
+
+AUTH = (os.environ.get("DCC_API_KEY"), os.environ.get("DCC_SECRET_KEY"))
+BASE_URL = 'https://www.encodeproject.org/{}/?format=json'
+ENCODE4_BULK_RNA_PIPELINES = [
+    '/pipelines/ENCPL862USL/',
+]
+
+
+def check_encode4_bulk_rna_pipeline(exp_acc):
+    experiment = requests.get(BASE_URL.format(exp_acc), auth=AUTH).json()
+
+    print('------------------------------')
+    print(exp_acc)
+    print('------------------------------')
+    bad_reason = []
+    serious_audits = {
+        'ERROR': len(experiment['audit'].get('ERROR', [])),
+        'NOT_COMPLIANT': len(experiment['audit'].get('NOT_COMPLIANT', [])),
+    }
+    if serious_audits['ERROR']:
+        print('Has {} ERROR audits'.format(serious_audits['ERROR']))
+    if serious_audits['NOT_COMPLIANT']:
+        print('Has {} NOT_COMPLIANT audits'.format(
+            serious_audits['NOT_COMPLIANT']
+        ))
+    print('Number of original files: {}'.format(
+        len(experiment['original_files'])
+    ))
+    analysisObj = experiment.get('analysis_objects', [])
+    print('Number of analyses: {}'.format(len(analysisObj)))
+    print('File count in analyses: {}'.format(list(
+        len(analysis['files']) for analysis in analysisObj
+    )))
+    skipped_analyses_count = 0
+    rep_count = len({
+        rep['biological_replicate_number']
+        for rep in experiment['replicates']
+    })
+    file_output_map = {}
+    expected_file_output_count = {
+        'transcriptome alignments': rep_count,
+        'alignments': rep_count,
+        'transcript quantifications': rep_count * 2,
+        'minus strand signal of unique reads': rep_count,
+        'plus strand signal of unique reads': rep_count,
+        'minus strand signal of all reads': rep_count,
+        'plus strand signal of all reads': rep_count,
+        'gene quantifications': rep_count,
+    }
+
+    for analysis in analysisObj:
+        if sorted(analysis['pipelines']) != ENCODE4_BULK_RNA_PIPELINES:
+            skipped_analyses_count += 1
+            continue
+        if analysis.get('assembly') not in ['GRCh38', 'mm10']:
+            print('Wrong assembly')
+            bad_reason.append('Wrong assembly')
+        if analysis.get('genome_annotation') not in ['V29', 'V21']:
+            print('Wrong genome annotation')
+            bad_reason.append('Wrong genome annotation')
+        for fid in analysis['files']:
+            f_obj = requests.get(BASE_URL.format(fid), auth=AUTH).json()
+            file_output_map.setdefault(f_obj['output_type'], 0)
+            file_output_map[f_obj['output_type']] += 1
+        if file_output_map != expected_file_output_count:
+            print('Wrong file output type map')
+            bad_reason.append('Wrong file output type map')
+            print('Has {}'.format(str(file_output_map)))
+            print('Expect {}'.format(str(expected_file_output_count)))
+    if skipped_analyses_count == len(analysisObj):
+        print('No ENCODE4 analysis found')
+        bad_reason.append('No ENCODE4 analysis found')
+    elif skipped_analyses_count:
+        print('Skipped {} non-ENCODE4 uniform analyses'.format(
+            skipped_analyses_count
+        ))
+    print('')
+    return bad_reason, serious_audits
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description='Script to check result of ENCODE4 bulk RNA-seq processing on '
+        'the ENCODE portal'
+    )
+    parser.add_argument(
+        'exp_accs',
+        nargs='*',
+        default=sys.stdin,
+        help='One or more experiment accessions (ENCSRs).'
+    )
+    parser.add_argument(
+        '--ticket', dest='ticket',
+        help='related ticket number (PROD###)'
+    )
+    return parser
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    summary = {}
+    GoodExperiments = {}
+    for exp_acc in args.exp_accs:
+        bad_reason, serious_audits = check_encode4_bulk_rna_pipeline(
+            exp_acc.strip()
+        )
+        status = ', '.join(bad_reason) or 'Good'
+        if status == 'Good':
+            experimentID = exp_acc.strip()
+            GoodExperiments[experimentID] = sum(serious_audits.values())
+        if sum(serious_audits.values()):
+            status += ' BUT has {} ERROR and {} NOT_COMPLIANT'.format(
+                serious_audits.get('ERROR', 0),
+                serious_audits.get('NOT_COMPLIANT', 0),
+            )
+
+        summary[exp_acc.strip()] = status
+
+    for exp_acc in summary:
+        print('{}: {}'.format(exp_acc, summary[exp_acc]))
+
+    if GoodExperiments:
+        if args.ticket:
+            release_filename = '%s_releasedPatch.txt' % (args.ticket).strip()
+            problem_filename = '%s_internalstatusPatch.txt' % (args.ticket).strip()
+        else:
+            release_filename = 'releasedPatch.txt'
+            problem_filename = 'internalstatusPatch.txt'
+
+        releasedFiles = open(release_filename, 'w+')
+        problemFiles = open(problem_filename, 'w+')
+        problemWriter = csv.writer(problemFiles, delimiter='\t')
+        problemWriter.writerow(['record_id', 'internal_status'])
+
+        for key in GoodExperiments:
+            if GoodExperiments[key]:
+                problemWriter.writerow([key, 'post-pipeline review'])
+            else:
+                releasedFiles.write(key)
+                releasedFiles.write('\n')
+                problemWriter.writerow([key, 'release ready'])
+
+
+if __name__ == '__main__':
+    main()
